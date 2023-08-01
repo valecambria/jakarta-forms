77f836eb695cc90560c13914ded58e4c5391c109
vale <vale.cambria@hotmail.com>
2023-08-01 18:16:14 -0300

    manejo de formularios con jakarta

---
+++ src/main/java/apiservlet/webapp/form/FormServlet.java
@@ -0,0 +1,38 @@
+package apiservlet.webapp.form;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/registro")
+public class FormServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        String email = req.getParameter("email");
+        try (PrintWriter out = resp.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("     <head>");
+            out.println("     <meta charset=\"UTF-8\">");
+            out.println("     <title>Resultado form</title>");
+            out.println("     </head>");
+            out.println("     <body>");
+            out.println("         <h1>Resultado form</h1>");
+            out.println("         <ul>");
+            out.println("           <li>Username: " + username +  "</li>");
+            out.println("           <li>Password: " + password +  "</li>");
+            out.println("           <li>Email: " + email +  "</li>");
+            out.println("         </ul>");
+            out.println("     </body>");
+            out.println("</html>");
+        }
+    }
+}
